e0ece9036bd11d0f5655e6ddfd46037e4bdad489
JQY18 <3165973561@qq.com>
2024-11-04 08:47:29 +0800

    fisrt-commit

---
+++ src/main/java/edu/hunnu/intermpracticebackend/IntermPracticeBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.hunnu.intermpracticebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntermPracticeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntermPracticeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hunnu/intermpracticebackend/IntermPracticeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hunnu.intermpracticebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntermPracticeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
